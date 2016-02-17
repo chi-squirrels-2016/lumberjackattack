a25b1d8a1ca39966a324a917bd14184caa361c6b
James Artz & TSLarson <pair+jelliotartz.tslarson@devbootcamp.com>
2016-02-17 17:26:34 -0600

    Add login conditional for login/logout button

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,9 +14,13 @@ post '/login' do
   user = User.authenticate(params[:email], params[:password])
   if user
     session[:user_id] = user.id
-    redirect "/users/#{user.id}/questions"
+    redirect "/questions"
   else
     redirect "/"
   end
 end
 
+delete '/logout' do
+  session.clear[:user_id]
+  redirect "/login"
+end
