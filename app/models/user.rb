14bf49b24085b240cc39bd8f716d146462acdcb9
Lauren Reid <lauren.g.reid@gmail.com>
2016-02-17 11:32:00 -0600

    Add bcrypt stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
-  has_many :answers 
+  has_many :answers
   has_many :questions
   has_many :votes
   has_many :comments
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(username,password)
+    self.username == username && self.password == password
+  end
+
 end
