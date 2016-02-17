4503da41a25334240c227b55b117dd546550d5e8
James Artz & TSLarson <pair+jelliotartz.tslarson@devbootcamp.com>
2016-02-17 16:29:23 -0600

    Complete Login and sign up page. Create Current User helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,30 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :votes
   has_many :comments
+  validates :email, :presence => true, uniqueness: true
+  validate :password_exist
+  validates :username, :presence => true, uniqueness: true
 
   def password
     @password ||= BCrypt::Password.new(password_hash)
   end
 
   def password=(new_password)
+    @test_password = new_password
     @password = BCrypt::Password.create(new_password)
     self.password_hash = @password
   end
 
-  def authenticate(username,password)
-    self.username == username && self.password == password
+  def password_exist
+    if @test_password.nil?
+      errors.add(:base, "You must set a password")
+    end
+  end
+
+  def self.authenticate(email,password)
+    user = self.find_by_email(email)
+    return nil unless user
+    user.password == password ? user : nil
   end
 
 end
