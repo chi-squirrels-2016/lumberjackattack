e9b90a8a817e3a5bb7b45e1e33b605347cc4e569
James Artz & TSLarson <pair+jelliotartz.tslarson@devbootcamp.com>
2016-02-18 11:33:27 -0600

    add comment box to question

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -18,6 +18,20 @@ get "/questions/:id" do
 
   erb :"/questions/show"
 end
+
+post "/questions/:question_id/comment" do
+  user = User.find(session[:user_id])
+  question = Question.find(params[:question_id])
+  comment = question.comments.create(content: params[:content], user: user)
+  if request.xhr?
+    content_type :json
+    {content: content, username: user.username}.to_json
+  else
+    redirect "/questions/#{question.id}"
+  end
+end
+
+
 # when you click the add comment button, a comment form drops down, the add comment button disappears (magically)
 # if you don't add text to the comment form, it will not save and an error pops up
 # if you do add text, when you click the button, the comment is saved in the data base, it appears under the question (magically), and the add comment button reappears
--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-  $('#new_comment_button').click(function( event ) {
+  $('#new_comment_button').on("click", function( event ) {
     event.preventDefault();
     $("#new_answer_button").hide();
     $('<form id="new_comment" name="new_comment" action="#" method="post"></form>').appendTo('#question');
     $('<textarea placeholder="Your Comment" cols="30" rows="5"></textarea>').appendTo('#new_comment');
     $("<button id='button' type='submit'>Add Comment!</button>").appendTo('#new_comment');
-    $('#new_comment_button').hide();
 
+    var id = $(this).attr('href');
+    console.log(id);
+
+    $('#new_comment_button').hide();
      var comment = $('textarea').val();
+
      $.ajax ({
       type: "post",
-      url:
-      data: comment
-     })
+      url: "/questions/"+ id +"/comment",
+      dataType: "json",
+      data: {content: comment}
+     }).done( function ( response ){
+      console.log(response);
+
+     });
 
    });
 });
@@ -28,14 +36,14 @@ $(document).ready(function() {
 //       event.preventDefault();
 //       var comment = $('textarea').val();
 //       var author = $('input[name="authorName"]').val();
-      if (comment.length <= 0) {
-        $('#new_comment').remove();
-        $('#new_comment_button').show();
-      } else {
-        $('#new_comment').remove();
-        $('<li>'+comment+'<span class="author">'+author+'</span></li>').appendTo("#comment_list");
-        $('#new_comment_button').show();
-      }
+//       if (comment.length <= 0) {
+//         $('#new_comment').remove();
+//         $('#new_comment_button').show();
+//       } else {
+//         $('#new_comment').remove();
+//         $('<li>'+comment+'<span class="author">'+author+'</span></li>').appendTo("#comment_list");
+//         $('#new_comment_button').show();
+//       }
 
-  });
-});
+//   });
+// });
