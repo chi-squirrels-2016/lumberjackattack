ba883923152b838202bafc1740afa2b568afd550
Lauren Reid <lauren.g.reid@gmail.com>
2016-02-18 14:05:49 -0600

    Answers going into db, returning json

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -12,9 +12,18 @@ post "/questions" do
 	current_user.questions << @question
 	redirect "/questions/#{@question.id}"
 end
+
 get "/questions/:id" do
   @question = Question.find(params[:id])
   @answers = Answer.where(question_id: params[:id])
   erb :"/questions/show"
 end
 
+post "/questions/:id/answers" do
+  @answer = Answer.create(user_id: current_user.id, question_id: params[:id], content: params[:answer][:content])
+  if request.xhr?
+    return {content: @answer.content, id: @answer.id, user_id: @answer.user_id}.to_json
+  else
+    puts "You have failed this city"
+  end
+end
